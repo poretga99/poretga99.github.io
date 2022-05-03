3f78640619daabcd81d19c7fd10836066cf5b082
Martin Zukovec <martin.zukovec@cosylab.com>
2022-05-03 13:12:37 +0200

    Multi tracker

--- js/index.js
+++ js/index.js
@@ -7,6 +7,26 @@ let inputBuffer = null;
 let outputBuffer = null;
 let detector = null;
 
+class MouseLocation {
+    startX = 0;
+    startY = 0;
+    currX = 0;
+    currY = 0;
+    endX = 0;
+    endY = 0;
+    pressed = false;
+}
+class MouseROI {
+    x = 0;
+    y = 0;
+    width = 0;
+    height = 0;
+    set = false;
+}
+let roi = new MouseROI()
+let mouseLocation = new MouseLocation();
+let roi_idx = 0;
+
 function start() {
     console.log("Starting");
 
@@ -41,12 +61,13 @@ function start() {
 
     detector.setInputBuffer(inputBuffer.dataPtr, height, width);
     detector.setOutputBuffer(outputBuffer.dataPtr, height, width);
-    detector.initializeTrackerROI(200, 200, 100, 100);
+    //detector.initializeTrackerROI(100, 100, 80, 80, Module.CornerType.TR);
+    //detector.initializeTrackerROI(300, 300, 80, 80, Module.CornerType.TL);
 
     // Start the camera capture
     console.log("Initializing camera...");
+    initializeInputCanvasROIDrawing("canvasInput")
     startCamera(width, height, resolution, video, processFrame);
-
 
     function processFrame() {
 
@@ -55,11 +76,19 @@ function start() {
             inputBuffer.dataOnHeap = new Uint8Array(Module.HEAP8.buffer, inputBuffer.dataPtr, numBytes);
         }
 
+        // If mouse is pressed, draw ROI
+        if(mouseLocation.pressed) {
+            canvasInputCtx.beginPath();
+            canvasInputCtx.rect(mouseLocation.startX, mouseLocation.startY,
+                mouseLocation.currX - mouseLocation.startX, mouseLocation.currY - mouseLocation.startY);
+            canvasInputCtx.strokeStyle = "red";
+            canvasInputCtx.stroke();
+        }
+
         // Get the canvas image data and copy it to heap buffer
         inputBuffer.dataOnHeap.set(new Uint8Array(canvasInputCtx.getImageData(0, 0, width, height).data));
 
         detector.processFrame();
-        roi2d = detector.getCurrentROI();
 
         if(outputBuffer.dataOnHeap.length === 0) {
             console.log("Resetting the output buffer...");
@@ -69,10 +98,70 @@ function start() {
         // Copy image from the output buffer to the output canvas
         canvasOutputCtx.putImageData(new ImageData(new Uint8ClampedArray(outputBuffer.dataOnHeap), width, height), 0, 0);
 
-        // Draw initial ROI rectangle
-        canvasOutputCtx.beginPath();
-        canvasOutputCtx.rect(roi2d.x, roi2d.y, roi2d.width, roi2d.height);
-        canvasOutputCtx.strokeStyle = "red";
-        canvasOutputCtx.stroke();
     }
+
+    function initializeInputCanvasROIDrawing(canvasId)
+    {
+        let canvas = document.getElementById(canvasId)
+        canvas.addEventListener("mousedown", function(e) {
+            trackMouse('down', e)
+        }, false)
+
+        canvas.addEventListener("mousemove", function(e) {
+            trackMouse('move', e);
+        }, false)
+
+        canvas.addEventListener("mouseup", function(e) {
+            trackMouse('up', e)
+        }, false)
+    }
+
+    function trackMouse(event, e) {
+        if (event === 'down') {
+            mouseLocation.startX = e.clientX - canvasOutput.offsetLeft;
+            mouseLocation.startY = e.clientY - canvasOutput.offsetTop;
+            mouseLocation.currX = mouseLocation.startX;
+            mouseLocation.currY = mouseLocation.startY;
+            mouseLocation.pressed = true;
+        }
+        if (event === 'move' && mouseLocation.pressed) {
+            mouseLocation.currX = e.clientX - canvasOutput.offsetLeft;
+            mouseLocation.currY = e.clientY - canvasOutput.offsetTop;
+        }
+        if (event === 'up') {
+            mouseLocation.endX = mouseLocation.currX;
+            mouseLocation.endY = mouseLocation.currY;
+            mouseLocation.pressed = false;
+            roi.x = Math.min(mouseLocation.startX, mouseLocation.endX);
+            roi.y = Math.min(mouseLocation.startY, mouseLocation.endY);
+            roi.width = Math.abs(mouseLocation.startX - mouseLocation.endX);
+            roi.height = Math.abs(mouseLocation.startY - mouseLocation.endY);
+            roi.set = true;
+
+            if (roi_idx < 4){
+                let cornerType = null;
+                switch(roi_idx){
+                    case 0:
+                        cornerType = Module.CornerType.TL;
+                        break;
+                    case 1:
+                        cornerType = Module.CornerType.TR;
+                        break;
+                    case 2:
+                        cornerType = Module.CornerType.BL;
+                        break;
+                    case 3:
+                        cornerType = Module.CornerType.BR;
+                        break;
+                    default:
+                        cornerType = Module.CornerType.TL;
+                        break;
+                }
+                detector.initializeTrackerROI(roi.x, roi.y, roi.width, roi.height, cornerType);
+                roi_idx = roi_idx + 1;
+            }
+
+        }
+    }
+
 }
